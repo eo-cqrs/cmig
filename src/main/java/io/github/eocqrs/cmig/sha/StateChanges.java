e796b876ec40ce0aeca3850ec5aae6f72a3428ae
l3r8yJ <l3r8y@duck.com>
2023-08-15 16:02:08 +0300

    normalized state

--- src/main/java/io/github/eocqrs/cmig/sha/StateChanges.java
+++ src/main/java/io/github/eocqrs/cmig/sha/StateChanges.java
@@ -26,6 +26,7 @@ import io.github.eocqrs.cmig.meta.XpathList;
 import org.cactoos.Scalar;
 import org.cactoos.io.ResourceOf;
 import org.cactoos.list.ListOf;
+import org.cactoos.text.Normalized;
 import org.cactoos.text.TextOf;
 
 import java.util.List;
@@ -64,13 +65,15 @@ public final class StateChanges implements Scalar<List<String>> {
     final List<String> files = this.list.value();
     for (final String file : files) {
       final String content =
-        new TextOf(
-          new ResourceOf(
-            "%s/%s"
-              .formatted(
-                this.cmig,
-                file
-              )
+        new Normalized(
+          new TextOf(
+            new ResourceOf(
+              "%s/%s"
+                .formatted(
+                  this.cmig,
+                  file
+                )
+            )
           )
         ).asString();
       contents.add(content);
